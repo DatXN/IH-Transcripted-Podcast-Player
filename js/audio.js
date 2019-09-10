fa0626caa8a1f44ddec4f99c8a13e207a263c3d0
DatXN <datnguyen231@gmail.com>
2019-09-10 11:25:52 +0700

    Update audio control

---
+++ js/audio.js
@@ -0,0 +1,154 @@
+(function() {
+  var audioPlayer = document.querySelector(".green-audio-player");
+  var playPause = audioPlayer.querySelector("#playPause");
+  var playpauseBtn = audioPlayer.querySelector(".play-pause-btn");
+  var progress = $("#progressBar");
+  var sliders = audioPlayer.querySelectorAll(".slider");
+  var player = audioPlayer.querySelector("audio");
+  var currentTime = audioPlayer.querySelector(".current-time");
+  var totalTime = audioPlayer.querySelector(".total-time");
+  var speaker = audioPlayer.querySelector("#speaker");
+  var btnReplay = $(audioPlayer.querySelector(".btnReplay"));
+  var btnForward = $(audioPlayer.querySelector(".btnForward"));
+
+  var draggableClasses = ["pin"];
+  var currentlyDragged = null;
+  var noSleep = new NoSleep();
+
+  window.addEventListener("mousedown", function(event) {
+    if (!isDraggable(event.target)) return false;
+
+    currentlyDragged = event.target;
+    let handleMethod = currentlyDragged.dataset.method;
+
+    this.addEventListener("mousemove", window[handleMethod], false);
+
+    window.addEventListener(
+      "mouseup",
+      () => {
+        currentlyDragged = false;
+        window.removeEventListener("mousemove", window[handleMethod], false);
+      },
+      false
+    );
+  });
+
+  playpauseBtn.addEventListener("click", togglePlay);
+  player.addEventListener("timeupdate", updateProgress);
+  player.addEventListener("loadedmetadata", () => {
+    totalTime.textContent = formatTime(player.duration);
+  });
+  player.addEventListener("canplay", makePlay);
+  player.addEventListener("ended", function() {
+    $(playPause).html("play_arrow");
+    player.currentTime = 0;
+  });
+
+  sliders.forEach(slider => {
+    let pin = slider.querySelector(".pin");
+    slider.addEventListener("click", window[pin.dataset.method]);
+  });
+
+  function isDraggable(el) {
+    let canDrag = false;
+    let classes = Array.from(el.classList);
+    draggableClasses.forEach(draggable => {
+      if (classes.indexOf(draggable) !== -1) canDrag = true;
+    });
+    return canDrag;
+  }
+
+  function inRange(event) {
+    let rangeBox = getRangeBox(event);
+    let rect = rangeBox.getBoundingClientRect();
+    let direction = rangeBox.dataset.direction;
+    if (direction == "horizontal") {
+      var min = rangeBox.offsetLeft;
+      var max = min + rangeBox.offsetWidth;
+      if (event.clientX < min || event.clientX > max) return false;
+    } else {
+      var min = rect.top;
+      var max = min + rangeBox.offsetHeight;
+      if (event.clientY < min || event.clientY > max) return false;
+    }
+    return true;
+  }
+
+  function updateProgress() {
+    var current = player.currentTime;
+    var percent = (current / player.duration) * 100;
+    progress.css("width", percent + "%").attr("aria-valuenow", percent);
+    currentTime.textContent = formatTime(current);
+  }
+
+  function getRangeBox(event) {
+    let rangeBox = event.target;
+    let el = currentlyDragged;
+    if (event.type == "click" && isDraggable(event.target)) {
+      rangeBox = event.target.parentElement.parentElement;
+    }
+    if (event.type == "mousemove") {
+      rangeBox = el.parentElement.parentElement;
+    }
+    return rangeBox;
+  }
+
+  function getCoefficient(event) {
+    let slider = getRangeBox(event);
+    let rect = slider.getBoundingClientRect();
+    let K = 0;
+    if (slider.dataset.direction == "horizontal") {
+      let offsetX = event.clientX - slider.offsetLeft;
+      let width = slider.clientWidth;
+      K = offsetX / width;
+    } else if (slider.dataset.direction == "vertical") {
+      let height = slider.clientHeight;
+      var offsetY = event.clientY - rect.top;
+      K = 1 - offsetY / height;
+    }
+    return K;
+  }
+
+  function rewind(event) {
+    if (inRange(event)) {
+      player.currentTime = player.duration * getCoefficient(event);
+    }
+  }
+
+  function formatTime(time) {
+    var min = Math.floor(time / 60);
+    var sec = Math.floor(time % 60);
+    return min + ":" + (sec < 10 ? "0" + sec : sec);
+  }
+
+  function togglePlay() {
+    if (player.paused) {
+      $(playPause).html("pause");
+      noSleep.enable();
+      player.play();
+    } else {
+      $(playPause).html("play_arrow");
+      noSleep.disable();
+      player.pause();
+    }
+  }
+
+  function makePlay() {
+    playpauseBtn.style.display = "block";
+  }
+  //
+  btnReplay.on("click", function(e) {
+    e.preventDefault();
+    player.currentTime -= 10;
+  });
+  btnForward.on("click", function(e) {
+    e.preventDefault();
+    player.currentTime += 10;
+  });
+  $(".progressZone").on("click", function(e) {
+    player.currentTime = (player.duration * e.offsetX) / $(this).width();
+  });
+
+  function init() {}
+  init();
+})();
--- js/site.js
+++ js/site.js
@@ -1,180 +1,114 @@
-var audioPlayer = document.querySelector('.green-audio-player');
-var playPause = audioPlayer.querySelector('#playPause');
-var playpauseBtn = audioPlayer.querySelector('.play-pause-btn');
-var loading = audioPlayer.querySelector('.loading');
-var progress = audioPlayer.querySelector('.progress');
-var sliders = audioPlayer.querySelectorAll('.slider');
-var volumeBtn = audioPlayer.querySelector('.volume-btn');
-var volumeControls = audioPlayer.querySelector('.volume-controls');
-var volumeProgress = volumeControls.querySelector('.slider .progress');
-var player = audioPlayer.querySelector('audio');
-var currentTime = audioPlayer.querySelector('.current-time');
-var totalTime = audioPlayer.querySelector('.total-time');
-var speaker = audioPlayer.querySelector('#speaker');
+(function() {
+  var podCasts = [
+    {
+      Id: "003",
+      Description:
+        "Bootstrapping a SaaS Business to $30,000,000/year with David Hauser of Grasshopper",
+      AudioFile: "3-indiehackers-david-hauser-of-grasshopper.mp3",
+      TranscriptFile: "003.html"
+    },
+    {
+      Id: "043",
+      Description:
+        "Confronting Your Fears and Taking a Leap with Pieter Levels of Nomad List",
+      AudioFile: "043-pieter-levels-of-nomad-list.mp3",
+      TranscriptFile: "043.html"
+    }
+  ];
+  var $transcriptsPanel = $("#transcriptsPanel");
+  function initTranscriptDisplay() {
+    function findClosestTimestamp(timestampData, goal) {
+      var closest = 0;
+      var i = 0;
+      while (goal - timestampData[i] > 0) {
+        closest = timestampData[i];
+        i++;
+      }
+      var result = {
+        current: closest,
+        next: timestampData[i]
+      };
+      return result;
+    }
+    function countWords(str) {
+      return str.split(" ").filter(function(n) {
+        return n != "";
+      }).length;
+    }
+    //
+    var $audio = $("#audioPodcast");
+    var timestampData = $("span.episode-transcript__timestamp")
+      .map(function() {
+        return $(this).data("bt-time");
+      })
+      .get();
+    var closestTimestamp = null;
+    var currentClosestTimestamp = null;
+    var currentClosestTimestampDisplay = null;
+    var currentClosestTimestampDisplayParagraph = null;
+    var myInterval = null;
+    var currentWordIndex = 0;
 
-var draggableClasses = ['pin'];
-var currentlyDragged = null;
+    $("div.episode-transcript__speaking-turn").addClass("noDisplayTranscript");
+    console.log(timestampData);
+    //
+    var options = {
+      each: function(element) {
+        setTimeout(function() {
+          $(element).addClass("animate");
+        }, 250);
+      }
+    };
+    $audio.on("timeupdate", function(e) {
+      var currentPlaybackTime = $audio[0].currentTime;
+      var result = findClosestTimestamp(timestampData, currentPlaybackTime);
+      closestTimestamp = result.current;
+      var currentPlaybackOffset = currentPlaybackTime - closestTimestamp;
+      var currentTimespan = result.next - result.current;
+      // display transcript
+      if (currentClosestTimestamp - closestTimestamp != 0) {
+        currentWordIndex = 0;
+        if (!!myInterval) {
+          clearInterval(myInterval);
+        }
+        //
+        currentClosestTimestamp = closestTimestamp;
+        //
 
-window.addEventListener('mousedown', function (event) {
-
-  if (!isDraggable(event.target)) return false;
-
-  currentlyDragged = event.target;
-  let handleMethod = currentlyDragged.dataset.method;
-
-  this.addEventListener('mousemove', window[handleMethod], false);
-
-  window.addEventListener('mouseup', () => {
-    currentlyDragged = false;
-    window.removeEventListener('mousemove', window[handleMethod], false);
-  }, false);
-});
-
-playpauseBtn.addEventListener('click', togglePlay);
-player.addEventListener('timeupdate', updateProgress);
-player.addEventListener('volumechange', updateVolume);
-player.addEventListener('loadedmetadata', () => {
-  totalTime.textContent = formatTime(player.duration);
-});
-player.addEventListener('canplay', makePlay);
-player.addEventListener('ended', function () {
-  playPause.attributes.d.value = "M18 12L0 24V0";
-  player.currentTime = 0;
-});
-
-volumeBtn.addEventListener('click', () => {
-  volumeBtn.classList.toggle('open');
-  volumeControls.classList.toggle('hidden');
-});
-
-window.addEventListener('resize', directionAware);
-
-sliders.forEach(slider => {
-  let pin = slider.querySelector('.pin');
-  slider.addEventListener('click', window[pin.dataset.method]);
-});
-
-directionAware();
-
-function isDraggable(el) {
-  let canDrag = false;
-  let classes = Array.from(el.classList);
-  draggableClasses.forEach(draggable => {
-    if (classes.indexOf(draggable) !== -1)
-    canDrag = true;
+        if (!!currentClosestTimestampDisplay) {
+          currentClosestTimestampDisplay.addClass("noDisplayTranscript");
+          currentClosestTimestampDisplayParagraph.unmark(options);
+        }
+        currentClosestTimestampDisplay = $(
+          "a[href$='" + currentClosestTimestamp + "']"
+        ).parents(".episode-transcript__speaking-turn");
+        currentClosestTimestampDisplay.removeClass("noDisplayTranscript");
+        currentClosestTimestampDisplayParagraph = $(
+          currentClosestTimestampDisplay.find("p.episode-transcript__paragraph")
+        );
+      }
+      // Highlight
+      if (!!currentClosestTimestampDisplayParagraph) {
+        var numberOfWords = currentClosestTimestampDisplayParagraph.text()
+          .length;
+        var playbackRatio = currentPlaybackOffset / currentTimespan;
+        currentClosestTimestampDisplayParagraph.unmark(options);
+        currentClosestTimestampDisplayParagraph.markRanges(
+          [
+            {
+              start: 0,
+              length:
+                numberOfWords * (playbackRatio + 0.035) > numberOfWords
+                  ? numberOfWords
+                  : numberOfWords * (playbackRatio + 0.035)
+            }
+          ],
+          options
+        );
+      }
+    });
+  }
+  $transcriptsPanel.load("/transcripts/003.html", function() {
+    initTranscriptDisplay();
   });
-  return canDrag;
-}
-
-function inRange(event) {
-  let rangeBox = getRangeBox(event);
-  let rect = rangeBox.getBoundingClientRect();
-  let direction = rangeBox.dataset.direction;
-  if (direction == 'horizontal') {
-    var min = rangeBox.offsetLeft;
-    var max = min + rangeBox.offsetWidth;
-    if (event.clientX < min || event.clientX > max) return false;
-  } else {
-    var min = rect.top;
-    var max = min + rangeBox.offsetHeight;
-    if (event.clientY < min || event.clientY > max) return false;
-  }
-  return true;
-}
-
-function updateProgress() {
-  var current = player.currentTime;
-  var percent = current / player.duration * 100;
-  progress.style.width = percent + '%';
-
-  currentTime.textContent = formatTime(current);
-}
-
-function updateVolume() {
-  volumeProgress.style.height = player.volume * 100 + '%';
-  if (player.volume >= 0.5) {
-    speaker.attributes.d.value = 'M14.667 0v2.747c3.853 1.146 6.666 4.72 6.666 8.946 0 4.227-2.813 7.787-6.666 8.934v2.76C20 22.173 24 17.4 24 11.693 24 5.987 20 1.213 14.667 0zM18 11.693c0-2.36-1.333-4.386-3.333-5.373v10.707c2-.947 3.333-2.987 3.333-5.334zm-18-4v8h5.333L12 22.36V1.027L5.333 7.693H0z';
-  } else if (player.volume < 0.5 && player.volume > 0.05) {
-    speaker.attributes.d.value = 'M0 7.667v8h5.333L12 22.333V1L5.333 7.667M17.333 11.373C17.333 9.013 16 6.987 14 6v10.707c2-.947 3.333-2.987 3.333-5.334z';
-  } else if (player.volume <= 0.05) {
-    speaker.attributes.d.value = 'M0 7.667v8h5.333L12 22.333V1L5.333 7.667';
-  }
-}
-
-function getRangeBox(event) {
-  let rangeBox = event.target;
-  let el = currentlyDragged;
-  if (event.type == 'click' && isDraggable(event.target)) {
-    rangeBox = event.target.parentElement.parentElement;
-  }
-  if (event.type == 'mousemove') {
-    rangeBox = el.parentElement.parentElement;
-  }
-  return rangeBox;
-}
-
-function getCoefficient(event) {
-  let slider = getRangeBox(event);
-  let rect = slider.getBoundingClientRect();
-  let K = 0;
-  if (slider.dataset.direction == 'horizontal') {
-
-    let offsetX = event.clientX - slider.offsetLeft;
-    let width = slider.clientWidth;
-    K = offsetX / width;
-
-  } else if (slider.dataset.direction == 'vertical') {
-
-    let height = slider.clientHeight;
-    var offsetY = event.clientY - rect.top;
-    K = 1 - offsetY / height;
-
-  }
-  return K;
-}
-
-function rewind(event) {
-  if (inRange(event)) {
-    player.currentTime = player.duration * getCoefficient(event);
-  }
-}
-
-function changeVolume(event) {
-  if (inRange(event)) {
-    player.volume = getCoefficient(event);
-  }
-}
-
-function formatTime(time) {
-  var min = Math.floor(time / 60);
-  var sec = Math.floor(time % 60);
-  return min + ':' + (sec < 10 ? '0' + sec : sec);
-}
-
-function togglePlay() {
-  if (player.paused) {
-    playPause.attributes.d.value = "M0 0h6v24H0zM12 0h6v24h-6z";
-    player.play();
-  } else {
-    playPause.attributes.d.value = "M18 12L0 24V0";
-    player.pause();
-  }
-}
-
-function makePlay() {
-  playpauseBtn.style.display = 'block';
-  loading.style.display = 'none';
-}
-
-function directionAware() {
-  if (window.innerHeight < 250) {
-    volumeControls.style.bottom = '-54px';
-    volumeControls.style.left = '54px';
-  } else if (audioPlayer.offsetTop < 154) {
-    volumeControls.style.bottom = '-164px';
-    volumeControls.style.left = '-3px';
-  } else {
-    volumeControls.style.bottom = '52px';
-    volumeControls.style.left = '-3px';
-  }
-}
+})();
